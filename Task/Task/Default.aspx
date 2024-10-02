cfea387389c4ae70b758fc970cffe8174bd47bf9
PrabhaK <kprabha0710@gmail.com>
2024-10-02 13:08:47 +0530

    Added : Project Files

---
+++ Task/Task/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Task._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <asp:Label Text="Enter a Count Of Adult : " runat="server" />
+        <asp:TextBox ID="AdultCount" runat="server" />
+    </div>
+    <div>
+        <asp:Label Text="Enter a Count Of Children : " runat="server" />
+        <asp:TextBox runat="server" ID="ChildrenCount" />
+    </div>
+
+    <div>
+        <asp:Button ID="Button" class="buttonsave" runat="server" Text="View Room" OnClick="BookRoom" />
+    </div>
+    <div>
+        <asp:Label ID="Message" runat="server" />
+    </div>
+
+
+</asp:Content>
---
+++ Task/Task/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Task
+{
+	public partial class _Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+		public void BookRoom(object sender, EventArgs e)
+		{
+			int nAdult = int.Parse(AdultCount.Text);
+			int nChildren = int.Parse(ChildrenCount.Text);
+
+			int nTotalGuest = nAdult + nChildren;
+
+			if(nAdult > 0)
+			{
+				if(nTotalGuest <= 16)
+				{
+					bookingroom(nTotalGuest, nAdult, nChildren);
+				}
+				else
+				{
+					Message.Text = "Only 16 people are Allowed ";
+				}
+
+			}
+			else
+			{
+				Message.Text = "Adult must be there while you booking Room ";
+			}
+
+
+		}
+		public void bookingroom(int nTotalGuest, int nAdult, int nChildren)
+		{
+			if(nTotalGuest < 16)
+			{
+				if(nTotalGuest == 4)
+				{
+					Message.Text = $"Room 1 : Adult{nAdult} - Children{nChildren}";
+				}
+				else
+				{
+					int nRooms = nTotalGuest / 4;
+					int nAdulttemp = nAdult - nRooms;
+					int nChildrentemp = nChildren - nRooms;
+
+					for(int i = 1; i <= nRooms; i++)
+					{
+
+
+						Message.Text += Message.Text = $"Room{i} : Adult {nAdulttemp} - children{nChildrentemp}";
+
+						nAdult = nAdult - nAdulttemp;
+						nChildren = nChildren - nChildrentemp;
+
+						nAdulttemp = nAdult;
+						nChildrentemp = nChildren;
+
+					}
+				}
+			}
+		}
+	}
+}
